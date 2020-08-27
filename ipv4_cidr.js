68cf6b2c6bf7e066a1e242f861b9f44ee33858b3
kam96 <kbalutch@uci.edu>
2020-08-26 23:14:47 -0700

    Completed range functions

--- ipv4_cidr.js
+++ ipv4_cidr.js
@@ -4,24 +4,24 @@
 
 class CIDR
 /*
-  Assumes that the CIDR is passed in the correct format
+  Assumes that the CIDR address is passed in the correct format
   [0-255].[0-255].[0-255].[0-255]/[0-32]
 */
 {
   constructor(address)
   {
     this.address = address;
-    this.cidr_num = parseInt(address.split("/").pop());
+    this.mask_bits = parseInt(address.split("/").pop());
     this.octets = address.split("/")[0].split(".").map(function (x)
       {return parseInt(x, 10);});
   }
 
   get mask()
-  // Given the number of fixed bits, generates the corresponding mask 
+  // Given the number of masked bits, generates the corresponding mask
   {
     var mask_str = '';
 
-    for (let i = 0; i < this.cidr_num; i++)
+    for (let i = 0; i < this.mask_bits; i++)
     {
       mask_str = mask_str.concat('1');
     }
@@ -29,13 +29,37 @@ class CIDR
     mask_str = mask_str.padEnd(32, '0');
 
     return mask_str.match(/.{1,8}/g).map(function (x)
-      {return parseInt(x, 2);}).join('.');
+      {return parseInt(x, 2);});
   }
 
   get numberOfAddresses()
   // Includes the fixed network address and broadcast address
   {
-    return Math.pow(2, 32-this.cidr_num);
+    return Math.pow(2, 32-this.mask_bits);
+  }
+
+  get lowerRange()
+  {
+    return this.octets.map((x, index) =>
+      {return x & this.mask[index];});
+  }
+
+  get wildCardBits()
+  {
+    return this.octets.map((x,index) =>
+      {return 255 - this.mask[index];});
+  }
+
+  get upperRange()
+  {
+    return this.lowerRange.map((x,index) =>
+      {return x + this.wildCardBits[index];});
+  }
+
+  get range()
+  // Return the range of IP addresses defined by the CIDR address
+  {
+    return [this.lowerRange.join("."), this.upperRange.join(".")];
   }
 
   toString()
@@ -44,6 +68,7 @@ class CIDR
   }
 }
 
+// Might want to change this to a function that can be called
 var cidr_str = process.argv.slice(2).toString();
 var re = new RegExp(/^([0-9]{1,3}\.){3}[0-9]{1,3}(\/([0-9]|[1-2][0-9]|3[0-2]))?$/);
 
@@ -58,11 +83,9 @@ catch (err)
 }
 
 var cidr = new CIDR(cidr_str);
-console.log(cidr.mask);
-console.log(cidr.numberOfAddresses);
+console.log("Mask: ", cidr.mask);
+console.log("Addresses: ", cidr.numberOfAddresses);
+console.log("Range: ", cidr.range);
 
 // Tested object type
 // console.log(Object.prototype.toString.call(cidr));
-
-//cidr = cidr.replace(/[^0-9a-zA-Z]/g, ' ');
-//console.log(cidr);
